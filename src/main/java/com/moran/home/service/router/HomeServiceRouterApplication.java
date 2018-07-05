eff715566b01162c0d83969c3cfa78195e04b6a1
Rahul <raahool_16@rediffmail.com>
2018-07-05 15:12:24 +0530

    initial commit

---
+++ src/main/java/com/moran/home/service/router/HomeServiceRouterApplication.java
@@ -0,0 +1,12 @@
+package com.moran.home.service.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceRouterApplication.class, args);
+	}
+}
