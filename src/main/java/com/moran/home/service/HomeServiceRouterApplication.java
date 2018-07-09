62a277be13cb2697d4570670d544f437cf7fd3ad
Rahul <raahool_16@rediffmail.com>
2018-07-09 23:33:09 +0530

    Continued!!

--- src/main/java/com/moran/home/service/HomeServiceRouterApplication.java
+++ src/main/java/com/moran/home/service/HomeServiceRouterApplication.java
@@ -1,4 +1,4 @@
-package com.moran.home.service.router;
+package com.moran.home.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
